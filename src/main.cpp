294b044e8c4969ba075efc8635ea87023b9bd81a
nathandunk <nbd2@rice.edu>
2019-04-08 15:54:29 -0500

    added data saving code and updated includes

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <MEL/Math.hpp>
 #include <MEL/Daq/NI/MyRio/MyRio.hpp>
 #include <MEL/Communications/MelNet.hpp>
+#include <MEL/Logging/Csv.hpp>
 
 // use mel namespace so we can say foo() instead of mel::foo()
 using namespace mel;
@@ -42,6 +43,8 @@ int main() {
     myrio.mspC.encoder.enable_channel(0);
     // set units per count on encoder (get_position() will return radians, make 360.0/500.0 if degrees prefered)
     myrio.mspC.encoder[0].set_units_per_count(2 * PI / 500.0);
+    // zeros the encoder, meaning that its CURRENT position is 0 radians
+    myrio.mspC.encoder[0].zero();
     // configure MSPC DIO[1] to be output (all DIOs are inputs by default)
     myrio.mspC.DIO[1].set_direction(Out);
     // create MelNet so we can stream data to host PC
@@ -53,6 +56,12 @@ int main() {
     Timer timer(hertz(1000));
     // create a Time point t
     Time t = Time::Zero;
+    // headers for the csv file for datalogging
+    std::vector<std::string> header = {"Time (s)", "Write (V)", "Read (V)"};
+    // path to csv file (absolute_folder will be created in system root)
+    std::string filepath = "/home/admin/MECH488/groupXX.csv";
+    // vector of double vectors which will hold information to write
+    std::vector<std::vector<double>> data;
     // start and run our control loop until STOP is true
     while(!STOP) {
 
@@ -96,6 +105,8 @@ int main() {
         // !!! END YOUR CONTROL IMPLEMENTATION !!!
         // --------------------------------------------------------------------
 
+        //append data to a vector to use for datalogging
+        data.push_back({t.as_seconds(), volts_write, volts_read});
         // stream doubles of interest (add/remove variables if desired)
         mn.send_data({volts_write, volts_read, (double)raw_counts, radians});
         // set real world output values
@@ -104,6 +115,11 @@ int main() {
         t = timer.wait();
 
     }
+    // write the header
+    csv_write_row(filepath, header);    
+    // append the data
+    csv_append_rows(filepath, data);
+
     // return 0 to indicate success
     return 0;
 }
