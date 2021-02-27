2ab03405bcbeda99ea51fbe165dc1c92b3868a7b
epezent <epezent@rice.edu>
2021-02-26 18:55:04 -0600

    add new readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# MECH488
-See the wiki for more information. https://github.com/mahilab/MECH488/wiki
+## myRIO Hardware Setup (MECH 488 TAs only)
+
+- Install [LabVIEW myRIO Toolkit 2019 (32-bit)](https://www.ni.com/en-us/support/downloads/software-products/download.labview-myrio-toolkit.html#305910).
+- Connect the myRIO via USB to host PC and find the device in Measurement and Automation Explorer (MAX) under `Remote Systems`.
+- Ensure that the current firmware version is `7.0.0f0`. If it is not, click the `Update Firmware` button and install the latest firmware.
+- Ensure `Enable Secure Shell Serve (sshd)` is enabled. 
+- SFTP the FPGA bitfile from [mahi-daq/misc](https://github.com/mahilab/mahi-daq/tree/master/misc) to the myRIO device:
+```shell
+cd mahi-daq/misc
+sftp admin@172.22.11.2
+cd /var/local/natinst
+mkdir bitfiles
+cd bitfiles
+put NiFpga_MyRio1900Fpga60.lvbitx
+chmod 777 NiFpga_MyRio1900Fpga60.lvbitx
+```
+
+
--- cmake/nilrt-arm-toolchain.cmake
+++ cmake/nilrt-arm-toolchain.cmake
@@ -4,12 +4,12 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
-# set flags for MEL CMakeLists.txt
+# set flags
 set(NI_LRT ON)
 set(NI_ARM ON)
 
 # root to cross compiler
-set(NI_ARM_ROOT "/dev/nilrt-arm" CACHE FILEPATH "Absolute path to NI Linux Realtime GNU toolchain (ARM).")
+set(NI_ARM_ROOT "C:/dev/nilrt-arm" CACHE FILEPATH "Absolute path to NI Linux Realtime GNU toolchain (ARM).")
 
 # add compiler program path
 LIST(APPEND CMAKE_PROGRAM_PATH ${NI_ARM_ROOT}/sysroots/i686-nilrtsdk-mingw32/usr/bin/arm-nilrt-linux-gnueabi)
@@ -24,8 +24,8 @@ set(CMAKE_CROSSCOMPILING 1)
 # setup sysroot (for NI RT cross compiler)
 set(CMAKE_SYSROOT ${NI_ARM_ROOT}/sysroots/cortexa9-vfpv3-nilrt-linux-gnueabi CACHE FILEPATH "Path to NI ARM Cross Compiler Sysroot")
 set(CMAKE_FIND_ROOT_PATH 
-    "C:/Program Files (x86)/MEL-nilrt-arm/lib/cmake/MEL"
-    "C:/Program Files/MEL-nilrt-arm/lib/cmake/MEL"
+    "C:/Program Files (x86)/mahi-daq-nilrt-arm/lib/cmake/mahi-daq"
+    "C:/Program Files/mahi-daq-nilrt-arm/lib/cmake/mahi-daq"
 )
 
 set(CMAKE_C_FLAGS_INIT   "-pthread -march=armv7-a -mfpu=vfpv3 -mfloat-abi=softfp -mcpu=cortex-a9")
